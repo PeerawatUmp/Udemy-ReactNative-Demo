c1002f607120268924003c222abd550488e0cbc6
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-12-29 14:52:06 +0700

    exercise-reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState,useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state,action) => {
+  switch (action.type) {
+    case 'increase':
+      return {...state,counter: state.counter + 1}
+    case 'decrease':
+      return {...state,counter: state.counter - 1}
+    default:
+      return state;
+  }
+}
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state,dispatch] = useReducer(reducer, {counter:0})
+  const {counter} = state;
 
   return (
     <View>
       <Button
         title="Increase"
-        onPress={() => {
+        onPress={() => 
           // Don't do this!
           // counter++;
           // counter = counter + 1;
-          setCounter(counter + 1);
-        }}
+          // setCounter(counter + 1);
+          dispatch({ type: 'increase'})
+        }
       />
       <Button
         title="Decrease"
-        onPress={() => {
-          setCounter(counter - 1);
-        }}
+        onPress={() => 
+          // setCounter(counter - 1);
+          dispatch({ type: 'decrease'})
+        }
       />
       <Text>Current Count: {counter}</Text>
     </View>
